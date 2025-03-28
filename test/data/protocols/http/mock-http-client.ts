1b793f96925de44e0759d89f08b2ae0a4570f49e
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-03-27 22:07:33 -0300

    test: create HttpClientSpy class

---
+++ test/data/protocols/http/mock-http-client.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpRequest, HttpResponse, HttpStatusCode } from "@src/data/protocols/http";
+
+export class HttpClientSpy<T = any> implements HttpClient {
+    url?: string;
+    method?: string;
+    body?: any;
+    headers?: any;
+    response: HttpResponse<T> = {
+        statusCode: HttpStatusCode.Ok
+    }
+
+    async request(params: HttpRequest): Promise<{ statusCode: HttpStatusCode; body?: any; }> {
+        const { method, url, body, headers } = params;
+
+        this.url = url;
+        this.method = method;
+        this.body = body;
+        this.headers = headers;
+
+        return this.response;
+    }
+}
