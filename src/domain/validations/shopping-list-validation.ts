ead822a6c0405248d376bc7ffab12e6b5269a25e
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-05 19:38:41 -0300

    feat: error messages validation

--- src/domain/validations/shopping-list-validation.ts
+++ src/domain/validations/shopping-list-validation.ts
@@ -1,5 +1,11 @@
 import { CreateShoppingListProps } from "../models";
 
+export enum ShoppingListCreateMessageRequire {
+    NAME = 'O campo name deve ser preenchido',
+    AMOUNT = 'O campo amount deve ser preenchido',
+    TYPE_AMOUNT = 'O campo typeAmount deve ser preenchido'
+}
+
 export interface IShoppingListValidation {
     create(params: CreateShoppingListProps): void;
     getByName(name: string): void;
