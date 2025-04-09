a52a5f205f157e48ce93cf5090d281764f9e549d
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-08 21:55:16 -0300

    feat: add ID in enum validation

--- src/domain/validations/shopping-list-validation.ts
+++ src/domain/validations/shopping-list-validation.ts
@@ -1,18 +1,18 @@
 import { CreateShoppingListProps } from "../models";
 
 export enum ShoppingListMessageRequire {
+    ID = 'O campo id deve ser preenchido',
     NAME = 'O campo name deve ser preenchido',
     AMOUNT = 'O campo amount deve ser preenchido',
     TYPE_AMOUNT = 'O campo typeAmount deve ser preenchido'
 }
 
 export enum ShoppingListMessageType {
+    ID = 'Campo id deve ser do tipo guid',
     NAME = 'Campo name deve ser do tipo string',
     AMOUNT = 'Campo amount deve ser do tipo number',
     TYPE_AMOUNT = 'Tipo não reconhecido por typeAmount'
 }
-
-
 export interface IShoppingListValidation {
     create(params: CreateShoppingListProps): void;
     getByName(name: string): void;
