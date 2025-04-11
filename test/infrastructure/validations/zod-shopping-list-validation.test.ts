5aa39205b6bbbf2b0a16521d68ea7f7d16142a5c
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-11 18:24:11 -0300

    test: ZodShoppingListValidation - Should be error whitout name create method

---
+++ test/infrastructure/validations/zod-shopping-list-validation.test.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from "@src/domain/errors";
+import { ShoppingListMessageRequire } from "@src/domain/validations";
+import { ZodShoppingListValidation } from "@src/infrastructure/validations";
+import { makeCreateShoppingList } from "@test/domain/mocks/shopping-list";
+import { describe, expect, test } from "vitest";
+
+const makeSut = () => new ZodShoppingListValidation();
+
+describe('ZodShoppingListValidation', () => {
+    test('Should be error whitout name create method', () => {
+        const sut = makeSut();
+        const request = makeCreateShoppingList();
+        // @ts-expect-error
+        delete request.name;
+
+        expect(() => sut.create(request)).toThrow(new ValidationError(ShoppingListMessageRequire.NAME));
+    });
+})
