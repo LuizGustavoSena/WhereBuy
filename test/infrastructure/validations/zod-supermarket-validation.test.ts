98d79aca5e67ec90f8ac2f695f90bc539d4b172f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-18 22:52:43 -0300

    test: ZodSupermarketValidation - Should be error whitout param in getProductByName method

---
+++ test/infrastructure/validations/zod-supermarket-validation.test.ts
@@ -0,0 +1,14 @@
+import { ValidationError } from "@src/domain/errors";
+import { SupermarketMessageRequire } from "@src/domain/validations/supermarket-validation";
+import { ZodSupermarketValidation } from "@src/infrastructure/validations";
+import { describe, expect, test } from "vitest";
+
+const makeSut = () => new ZodSupermarketValidation();
+
+describe('ZodSupermarketValidation', () => {
+    test('Should be error whitout param in getProductByName method', () => {
+        const sut = makeSut();
+        // @ts-expect-error
+        expect(() => sut.getProductByName()).toThrow(new ValidationError(SupermarketMessageRequire.NAME));
+    });
+})
