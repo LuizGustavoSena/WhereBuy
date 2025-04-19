876c545ce5e7e9cdf729ae3b4b20604064514dac
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-18 22:54:18 -0300

    test: ZodSupermarketValidation - Should be error when another param type in getProductByName method

--- test/infrastructure/validations/zod-supermarket-validation.test.ts
+++ test/infrastructure/validations/zod-supermarket-validation.test.ts
@@ -1,5 +1,6 @@
+import { faker } from "@faker-js/faker";
 import { ValidationError } from "@src/domain/errors";
-import { SupermarketMessageRequire } from "@src/domain/validations/supermarket-validation";
+import { SupermarketMessageRequire, SupermarketMessageType } from "@src/domain/validations/supermarket-validation";
 import { ZodSupermarketValidation } from "@src/infrastructure/validations";
 import { describe, expect, test } from "vitest";
 
@@ -10,5 +11,11 @@ describe('ZodSupermarketValidation', () => {
         const sut = makeSut();
         // @ts-expect-error
         expect(() => sut.getProductByName()).toThrow(new ValidationError(SupermarketMessageRequire.NAME));
+    });
+    
+    test('Should be error when another param type in getProductByName method', () => {
+        const sut = makeSut();
+        // @ts-expect-error
+        expect(() => sut.getProductByName(faker.number.int())).toThrow(new ValidationError(SupermarketMessageType.NAME));
     });
 })
