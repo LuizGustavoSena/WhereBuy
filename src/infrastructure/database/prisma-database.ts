11b9168c2bc00a45ead1a51e5b9a622c94e5ba03
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-22 21:43:06 -0300

    feat: PrimaDatabase class create method

---
+++ src/infrastructure/database/prisma-database.ts
@@ -0,0 +1,35 @@
+import { IDatabaseClient, ParamsUpdateDatabaseClient } from "@src/data/protocols/database";
+import { ShoppingListProps } from "@src/domain/models";
+import { PrismaClient } from '../../../generated/prisma';
+
+export class PrimaDatabase implements IDatabaseClient<ShoppingListProps> {
+    prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
+    }
+
+    async create(params: ShoppingListProps): Promise<ShoppingListProps> {
+        const response = await this.prisma.shoppingList
+            .create({ data: params});
+
+        return response as ShoppingListProps;
+    }
+
+    async getAllByUserId(userId: string): Promise<ShoppingListProps[]> {
+        throw new Error("Method not implemented.");
+    }
+    
+    async getByFIlter(filter: object): Promise<ShoppingListProps[]> {
+        throw new Error("Method not implemented.");
+    }
+    
+    async update(params: ParamsUpdateDatabaseClient<ShoppingListProps>): Promise<ShoppingListProps> {
+        throw new Error("Method not implemented.");
+    }
+    
+    async deleteById(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
