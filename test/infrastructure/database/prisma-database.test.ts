0abde1528e5dc9fc1be74cd0acae4c36db35d113
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-25 17:16:46 -0300

    test: PrismaDatabase - Should be successful update a shoppingList item

--- test/infrastructure/database/prisma-database.test.ts
+++ test/infrastructure/database/prisma-database.test.ts
@@ -1,13 +1,23 @@
 import { faker } from "@faker-js/faker";
 import { PrismaDatabase } from "@src/infrastructure/database";
 import { makeShoppingListItem } from "@test/domain/mocks/shopping-list";
-import { describe, expect, test } from "vitest";
+import { afterAll, describe, expect, test } from "vitest";
 
 const sut = new PrismaDatabase();
 
 describe('PrismaDatabase', () => {
     var id = faker.string.uuid();
     var userId = faker.string.uuid();
+
+    afterAll(async () => {
+        const response = await sut.getByFIlter({});
+
+        const removeItens = response.filter(el => el.name.includes('-test'));
+
+        await Promise.all(removeItens.map(async (el) => {
+            await sut.deleteById(el.id);
+        }));
+    });
 
     test('Should be successful create a shoppingList item', async () => {
         const shoppingList = makeShoppingListItem({ id, userId });
@@ -31,5 +41,16 @@ describe('PrismaDatabase', () => {
         const response = await sut.getByFIlter({ id });
 
         expect(response).toHaveLength(1);
+    });
+
+    test('Should be successful update a shoppingList item', async () => {
+        const name = `${faker.commerce.productName()}-test`;
+
+        const response = await sut.update({
+            id,
+            data: { name }
+        });
+
+        expect(response.name).toBe(name);
     });
 });
