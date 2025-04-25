0e264b8be9504cb8831e27ad86506c51ec503fc6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-24 21:46:05 -0300

    fix: amount value and created type

--- test/domain/mocks/shopping-list/mocks-shopping-list.ts
+++ test/domain/mocks/shopping-list/mocks-shopping-list.ts
@@ -11,8 +11,8 @@ export const makeCreateShoppingList = (params?: Partial<CreateShoppingListProps>
 export const makeShoppingListItem = (params?: Partial<ShoppingListProps>): ShoppingListProps => ({
     id: params?.id ? params.id : faker.string.uuid(),
     name: params?.name ? params.name : faker.commerce.productName(),
-    amount: params?.amount ? params.amount : faker.number.int(),
+    amount: params?.amount ? params.amount : Number(faker.commerce.price({dec: 2})),
     typeAmount: params?.typeAmount ? params.typeAmount : faker.helpers.enumValue(TypeAmountEnum),
-    created: params?.created ? params.created : faker.date.anytime().toISOString(),
+    created: params?.created ? params.created : faker.date.anytime(),
     userId: params?.userId ? params.userId : faker.string.uuid()
 });
