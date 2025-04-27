f0458ad8fe6551446954e1589ba83f4ef16d47cf
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-26 22:05:41 -0300

    fix: faker value to amount to int

--- test/domain/mocks/shopping-list/mocks-shopping-list.ts
+++ test/domain/mocks/shopping-list/mocks-shopping-list.ts
@@ -3,7 +3,7 @@ import { CreateShoppingListProps, ShoppingListProps, TypeAmountEnum } from "@src
 
 export const makeCreateShoppingList = (params?: Partial<CreateShoppingListProps>): CreateShoppingListProps => ({
     name: params?.name ? params.name : faker.commerce.productName(),
-    amount: params?.amount ? params.amount : faker.number.int(),
+    amount: params?.amount ? params.amount : faker.number.int({ max: 20 }),
     typeAmount: params?.typeAmount ? params.typeAmount : faker.helpers.enumValue(TypeAmountEnum),
     userId: params?.userId ? params.userId : faker.string.uuid()
 });
@@ -11,7 +11,7 @@ export const makeCreateShoppingList = (params?: Partial<CreateShoppingListProps>
 export const makeShoppingListItem = (params?: Partial<ShoppingListProps>): ShoppingListProps => ({
     id: params?.id ? params.id : faker.string.uuid(),
     name: params?.name ? params.name : `${faker.commerce.productName()}-test`,
-    amount: params?.amount ? params.amount : Number(faker.commerce.price({dec: 2})),
+    amount: params?.amount ? params.amount : faker.number.int({ max: 20 }),
     typeAmount: params?.typeAmount ? params.typeAmount : faker.helpers.enumValue(TypeAmountEnum),
     created: params?.created ? params.created : faker.date.anytime(),
     userId: params?.userId ? params.userId : faker.string.uuid()
