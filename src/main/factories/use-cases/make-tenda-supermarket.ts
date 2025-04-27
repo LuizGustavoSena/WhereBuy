ee0994bde315c9e4aff6eb4cff71764418f6411b
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-27 13:36:52 -0300

    fix: imports makeTendaSupermarket

--- src/main/factories/use-cases/make-tenda-supermarket.ts
+++ src/main/factories/use-cases/make-tenda-supermarket.ts
@@ -1,5 +1,5 @@
 import { TendaSupermarket } from "@src/data/use-cases/tenda-supermarket";
-import { makeAxiosHttpClient } from "../infrastructure/http/make-axios-http-client";
+import { makeAxiosHttpClient } from "../infrastructure";
 
 export const makeTendaSupermarket = () : TendaSupermarket =>
     new TendaSupermarket(makeAxiosHttpClient());
