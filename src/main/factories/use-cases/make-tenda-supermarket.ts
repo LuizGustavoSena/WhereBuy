65e2bba8cb61c4388b5a46e5edd39f5425635cdf
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-27 13:26:21 -0300

    fix: adjust makeTendaSupermarket function

--- src/main/factories/use-cases/make-tenda-supermarket.ts
+++ src/main/factories/use-cases/make-tenda-supermarket.ts
@@ -1,19 +1,5 @@
 import { TendaSupermarket } from "@src/data/use-cases/tenda-supermarket";
-import { ISupermarket } from "@src/domain/use-cases";
-import { MakeAxiosHttpClient } from "../infrastructure/http/make-axios-http-client";
+import { makeAxiosHttpClient } from "../infrastructure/http/make-axios-http-client";
 
-export class MakeTendaSupermarket {
-    private static instance: ISupermarket;
-
-    private constructor() { }
-
-    static getInstance(): ISupermarket {
-        if (!MakeTendaSupermarket.instance) {
-            const httpClient = MakeAxiosHttpClient.getInstance();
-
-            MakeTendaSupermarket.instance = new TendaSupermarket(httpClient);
-        }
-
-        return MakeTendaSupermarket.instance;
-    }
-}
+export const makeTendaSupermarket = () : TendaSupermarket =>
+    new TendaSupermarket(makeAxiosHttpClient());
