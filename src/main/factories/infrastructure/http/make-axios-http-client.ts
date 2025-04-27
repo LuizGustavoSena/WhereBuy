e54c81ed24c3b38f92025e346136f4416c771892
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-27 13:26:31 -0300

    fix: adjust makeAxiosHttpClient

--- src/main/factories/infrastructure/http/make-axios-http-client.ts
+++ src/main/factories/infrastructure/http/make-axios-http-client.ts
@@ -1,15 +1,4 @@
-import { IHttpClient } from "@src/data/protocols/http";
 import { AxiosHttpClient } from "@src/infrastructure/http/axios-http-client";
 
-export class MakeAxiosHttpClient {
-    private static instance: IHttpClient;
-
-    private constructor() { }
-
-    static getInstance(): IHttpClient {
-        if (!MakeAxiosHttpClient.instance)
-            MakeAxiosHttpClient.instance = new AxiosHttpClient();
-
-        return MakeAxiosHttpClient.instance;
-    }
-}
+export const makeAxiosHttpClient= (): AxiosHttpClient  => 
+    new AxiosHttpClient();
