0a9ad5d76e96ca2d47063a6dfc292f56a592803b
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-26 22:31:56 -0300

    feat: UuidGuid class

---
+++ src/infrastructure/guid/uuid-guid.ts
@@ -0,0 +1,10 @@
+import { IGuidClient } from "@src/data/protocols/guid";
+import { v4 as uuidv4 } from 'uuid';
+
+export class UuidGuid implements IGuidClient {
+    constructor(){};
+    
+    generate(): string {
+        return uuidv4();
+    }
+}
