97780c8ca98555d1e885bf8c18d3964080f42482
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-29 23:50:18 -0300

    feat: run server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,13 @@
-console.log('hello world')
+import app from "./app";
+
+async function upServer() {
+    var port = Number(process.env.PORT) || 3000;
+
+    try {
+        await app.listen({ port });
+        console.log('Server is running on port ' + port);
+    } catch (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+}
