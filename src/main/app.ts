aa6d731e5f5bcfe3c5ed458ec25964cb2f9c47b8
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-30 21:25:28 -0300

    feat: register ShoppingListRouter

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
+import { ShoppingListRouter } from './routes';
 
 const app = fastify();
 
+app.register(ShoppingListRouter, { prefix: '/shopping-list' });
 
 export default app;
