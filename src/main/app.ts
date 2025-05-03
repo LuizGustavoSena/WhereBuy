0bcbe43997511516769ec55d591eb8cffc3ac02f
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-03 13:32:41 -0300

    feat: protected Router

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import authentication from './middlewares/protected-router';
 import ShoppingListRouter from './routes/shopping-list-route';
 
 const app = express();
 const protectedRoutes = express.Router();
 
 protectedRoutes.use('/shopping-list', ShoppingListRouter);
+
+app.use('/v1', authentication, protectedRoutes)
 
 export default app;
