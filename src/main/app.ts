7670e789c53cf2628ba3147416c06be6d3dc6cd8
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-05 18:35:46 -0300

    fix: read body requests

--- src/main/app.ts
+++ src/main/app.ts
@@ -5,6 +5,8 @@ import ShoppingListRouter from './routes/shopping-list-route';
 const app = express();
 const protectedRouter = express.Router();
 
+app.use(express.json());
+
 protectedRouter.use('/shopping-list', ShoppingListRouter);
 
 app.use('/v1', authentication, protectedRouter);
