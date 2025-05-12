491a772019e07bc467bdbba206dee4910c741743
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-12 18:25:26 -0300

    test: TendaSupermarketSpy

--- test/domain/mocks/tenda/mock-get-product.ts
+++ test/domain/mocks/tenda/mock-get-product.ts
@@ -1,5 +1,7 @@
 import { faker } from "@faker-js/faker";
+import { Product } from "@src/domain/models";
 import { GetProductProps } from "@src/domain/models/tenda";
+import { ISupermarket } from "@src/domain/use-cases";
 
 export const mockGetProducts = (): GetProductProps => ({
     products: [
@@ -11,3 +13,21 @@ export const mockGetProducts = (): GetProductProps => ({
         }
     ]
 });
+
+export const mockProducts = (params?: Partial<Product>): Product => ({
+    name: params && params.name ? params.name : faker.commerce.productName(),
+    urlPhoto: params && params.urlPhoto ? params.urlPhoto : faker.internet.url(),
+    price: params && params.price ? params.price : Number(faker.commerce.price()),
+    supermarket: 'Tenda Atacado'
+});
+
+export class TendaSupermarketSpy implements ISupermarket {
+    name: string;
+    response: Product[] = [mockProducts()];
+
+    async getProductByName(name: string): Promise<Product[]> {
+        this.name = name;
+
+        return this.response;
+    }
+}
