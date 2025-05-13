1fe0a7939ef2f541558fc807da9e9c7f848e9847
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-12 22:10:12 -0300

    test: RenderBuyList - Should be successful renderListByUserId

--- test/data/use-cases/render-buy-list.test.ts
+++ test/data/use-cases/render-buy-list.test.ts
@@ -1,7 +1,9 @@
+import { faker } from "@faker-js/faker";
 import { RenderBuyList } from "@src/data/use-cases";
 import { ISupermarket } from "@src/domain/use-cases";
-import { TendaSupermarketSpy } from "@test/domain/mocks/tenda";
-import { describe } from "vitest";
+import { makeShoppingListItem } from "@test/domain/mocks/shopping-list";
+import { mockProducts, TendaSupermarketSpy } from "@test/domain/mocks/tenda";
+import { describe, expect, test } from "vitest";
 import { IDatabaseClient } from "../protocols/database";
 import DatabaseSpy from "../protocols/database/mock-database-client";
 
@@ -23,5 +25,22 @@ const makeSut = (): Props => {
 }
 
 describe('RenderBuyList', () => {
+    test('Should be successful renderListByUserId', async () => {
+        const { sut, database, supermarket } = makeSut();
 
+        const shoppingList = [makeShoppingListItem()];
+        const supermarketList = [mockProducts(), mockProducts()];
+
+        database.getAllByUserId = async () => shoppingList;
+        // @ts-expect-error
+        supermarket.response = supermarketList;
+
+        const response = await sut.renderListByUserId(faker.string.uuid());
+
+        expect(response.products).toHaveLength(1);
+        expect(response.products[0]).toMatchObject({
+            name: shoppingList[0].name
+        });
+        expect(response.products[0].products).toEqual(supermarketList);
+    });
 });
