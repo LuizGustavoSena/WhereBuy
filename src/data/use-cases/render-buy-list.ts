ac5caad18a2f998aeb21ec838a194e363e1ee5b6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-13 21:53:14 -0300

    fix: catch error in renderListByUserId

--- src/data/use-cases/render-buy-list.ts
+++ src/data/use-cases/render-buy-list.ts
@@ -1,3 +1,4 @@
+import { RenderBuyListError } from "@src/domain/errors";
 import { renderListByUserIdResponse, ShoppingListProps } from "@src/domain/models";
 import { ISupermarket } from "@src/domain/use-cases";
 import { IRenderBuyList } from "@src/domain/use-cases/render-buy-list";
@@ -10,15 +11,19 @@ export class RenderBuyList implements IRenderBuyList {
     ) { };
 
     async renderListByUserId(userId: string): Promise<renderListByUserIdResponse> {
-        const items = await this.dbClient.getAllByUserId(userId);
+        try {
+            const items = await this.dbClient.getAllByUserId(userId);
 
-        const results = await Promise.all(items.map(item => this.tendaSupermarket.getProductByName(item.name)));
+            const results = await Promise.all(items.map(item => this.tendaSupermarket.getProductByName(item.name)));
 
-        return {
-            products: results.map((el, index) => ({
-                name: items[index].name,
-                products: el
-            }))
-        };
+            return {
+                products: results.map((el, index) => ({
+                    name: items[index].name,
+                    products: el
+                }))
+            };
+        } catch (error) {
+            throw new RenderBuyListError();
+        }
     }
 }
