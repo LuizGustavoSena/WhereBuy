44c244489fc5536d9a82ef82df53518d1497f042
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-05-15 21:56:20 -0300

    fix: receive the URLS_ENABLE_CORS array in env

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,15 +1,14 @@
 import { env } from '@src/infrastructure/config/env';
 import cors from 'cors';
 
-var allowlist = env.URLS_ENABLE_CORS.split(',');
-
-var corsOptionsDelegate = function (req: any, callback: Function) {
-    var corsOptions = { origin: false };
+const corsOptionsDelegate = function (req: any, callback: Function) {
+    const allowlist = env.URLS_ENABLE_CORS;
+    const corsOptions = { origin: false };
 
     if (env.NODE_ENV !== 'production') return callback(null, corsOptions)
 
     if (allowlist.indexOf(req.header('Origin')) !== -1)
-        corsOptions = { origin: true }
+        corsOptions.origin = true;
 
     callback(null, corsOptions);
 }
